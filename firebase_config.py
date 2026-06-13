77528d99d262f0e8f1f9b2f7391002653ebba707
Bima Aidil <bimaaidil208@gmail.com>
2026-06-13 15:29:01 +0700

    Fix: move tensorflow import inside load function to prevent vercel crash1

--- firebase_config.py
+++ firebase_config.py
@@ -1,14 +1,21 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
-import json
+import ast
 
 if not firebase_admin._apps:
-    # 1. Cek apakah ada environment variable dari Vercel
-    if os.environ.get('FIREBASE_CREDENTIALS'):
-        cred_json = json.loads(os.environ.get('FIREBASE_CREDENTIALS'))
-        cred = credentials.Certificate(cred_json)
-    # 2. Jika tidak ada (berarti sedang dijalankan di laptop lokal)
+    env_creds = os.environ.get('FIREBASE_CREDENTIALS')
+    
+    if env_creds:
+        try:
+            # Menggunakan ast.literal_eval sebagai pengganti json.loads 
+            # untuk membersihkan karakter newline (\n) atau spasi Windows yang rusak
+            cred_dict = ast.literal_eval(env_creds)
+            cred = credentials.Certificate(cred_dict)
+        except Exception as e:
+            print(f"⚠️ Gagal memproses format JSON: {e}")
+            # Jika ast gagal, biarkan sistem mencoba Certificate langsung
+            cred = credentials.Certificate("serviceAccountKey.json")
     else:
         cred = credentials.Certificate("serviceAccountKey.json")
         
